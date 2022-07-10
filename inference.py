218bfbcd948188a7a7ad27fbf3e6ed0d6d889348
moh295 <mohammed@clearimageai.com>
2022-07-11 00:56:30 +0200

    annoutation with opencv

--- inference.py
+++ inference.py
@@ -61,7 +61,7 @@ def inference_and_save_mobilnet_full_data(model,save_dir,images,tensors,count,la
             cv2.putText(draw, f'{category:s} {prob:.3f}', bbox[:2], font,1, color,2, cv2.LINE_AA)
 
 
-        cv2.imwrite(path_to_output_image + str(cnt) + '_demo_output.png',draw)
+        cv2.imwrite(path_to_output_image + str(cnt) + '.png',draw)
         print(f'Output image is saved to {path_to_output_image}{cnt}.png')
         cnt += 1
         # image.show()
