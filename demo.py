00139dfe65bd67634fa4c0f61f3579564c67f00b
moh295 <mohammed@clearimageai.com>
2022-07-10 02:12:49 +0200

    image dataloader

--- demo.py
+++ demo.py
@@ -92,8 +92,8 @@ if __name__ == '__main__':
     #image= demo_data_image()
     image=image = Image.open('demo_input.png')
     # to_tensor=transforms.ToTensor()
-    transforms.Compose(
-        [transforms.Resize(input_size),
+    transform=transforms.Compose(
+        [
          transforms.ToTensor(),
          transforms.Normalize(mean=[0.485, 0.456, 0.406],
                               std=[0.229, 0.224, 0.225]),
