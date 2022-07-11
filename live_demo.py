3a0629dc521596eac2c9a28cc7fea4377c0b550d
moh295 <mohammed@clearimageai.com>
2022-07-11 19:24:34 +0200

    image sacle

--- live_demo.py
+++ live_demo.py
@@ -105,10 +105,10 @@ if __name__ == '__main__':
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # # Display the resulting frame
-        # cv2.imshow('frame', frame)
+        cv2.imshow('frame', frame)
         # cv2.imshow('gray', gray)
-        # if cv2.waitKey(20) & 0xFF == ord('q'):
-        #     break
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
 
         image=image_resize(frame,args.scale)
         tensor = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
