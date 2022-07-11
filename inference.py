f6d0701baeb126f89e7959e73f066d729233566f
moh295 <mohammed@clearimageai.com>
2022-07-11 09:04:20 +0200

    image sacle

--- inference.py
+++ inference.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     checkpoint = '/App/data/torch_trained_fasterrcnn.pth'
     a = argparse.ArgumentParser()
     a.add_argument("--images", help="path to input images", default='data/images/')
-    a.add_argument("--scale", help="input image scale", default='data/images/')
+    a.add_argument("--scale", help="input image scale", default=0.5)
     a.add_argument("--output", help="path to output folder", default='data/output/')
     a.add_argument("--batch", help="batch size", default=30)
     a.add_argument("--checkpoint", help="train model weight", default=checkpoint)
