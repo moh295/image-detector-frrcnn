798a2121e52fd05804edce085aafb0a4e342b4ee
moh295 <mohammed@clearimageai.com>
2022-07-11 10:42:46 +0200

    image sacle

--- validation.py
+++ validation.py
@@ -73,7 +73,7 @@ def inference_and_save_mobilnet_full_data(model,save_dir,dataloder):
 if __name__ == '__main__':
     checkpoint = '/App/data/torch_trained_fasterrcnn.pth'
     a = argparse.ArgumentParser()
-    a.add_argument("--dataset", help="PSCAL VOC2007 format folder", default='data/hand_object_datasets/pascal_voc_format/VOCdevkit2007_handobj_100K/VOC2007')
+    a.add_argument("--dataset", help="PSCAL VOC2007 format folder", default='data/pascal_voc_format/VOCdevkit2007_handobj_100K/VOC2007')
     a.add_argument("--scale", help="input image scale", default=0.6)
     a.add_argument("--output", help="path to output folder", default='data/output/')
     a.add_argument("--batch", help="batch size", default=60)
