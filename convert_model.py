d4f9f52ffb909e2c2704e753c2ff8a6e830cf90f
moh295 <mohammed@clearimageai.com>
2022-07-14 12:51:09 +0200

    convert to onnx

--- convert_model.py
+++ convert_model.py
@@ -83,6 +83,6 @@ if __name__ == '__main__':
 
     batch_size=1
     print('batch size',batch_size)
-    x = [torch.rand(batch_size, 300, 400), torch.rand(batch_size, 500, 400)]
+    x = [torch.rand(batch_size, 300, 400).to(device), torch.rand(batch_size, 500, 400).to(device)]
     predictions = model(x)
     torch.onnx.export(model, x, "faster_rcnn.onnx", opset_version = 11)
