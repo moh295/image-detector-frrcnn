d8efe2732827f6172d032d065a6f35dc3728a5c9
moh295 <mohammed@clearimageai.com>
2022-07-19 17:52:40 +0200

    free vs contact hand annotation

--- live_demo.py
+++ live_demo.py
@@ -14,8 +14,9 @@ if __name__ == '__main__':
     # checkpoint = root + 'torch_trained_fasterrcnn_100p.pth'
     checkpoint=config["checkpoint"]
     a = argparse.ArgumentParser()
-    a.add_argument("--cam",type=int, help="webcam number e.g: 0 , 1", default=1)
-    a.add_argument("--scale",type=int, help="input image scale", default=0.6)
+    a.add_argument("--cam",type=int, help="webcam number e.g: 0 , 1", default=0)
+    a.add_argument("--input_scale",type=int, help="input image scale", default=0.6)
+    a.add_argument("--output_scale", type=int, help="input image scale", default=1)
     a.add_argument("--output", help="path to output folder", default=root+'output/')
     a.add_argument("--batch",type=int, help="batch size", default=1)
     a.add_argument("--checkpoint", help="train model weight", default=checkpoint)
@@ -23,6 +24,9 @@ if __name__ == '__main__':
     model=FrRCNN(checkpoint)
     #open cam
     cap = cv2.VideoCapture(args.cam)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    # cap.set(cv2.CAP_PROP_FPS, 30)
     print('batch size',args.batch)
     image_list = []
     tensor_list=[]
@@ -32,9 +36,9 @@ if __name__ == '__main__':
     while True:
 
         # Capture frame-by-frame
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
         ret, frame = cap.read()
+        print('frame size',frame.shape)
         # # Display the resulting frame
         # cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -46,7 +50,7 @@ if __name__ == '__main__':
         tensor_list.append(tensor)
         image_batch.append(image)
         if len(tensor_list)==args.batch :
-            model.predict(args.output, tensors=tensor_list, images=image_batch, show_vid=True)
+            model.predict(args.output, tensors=tensor_list, images=image_batch, show_vid=True,output_scale=args.output_scale)
             count+=args.batch
             tensor_list = []
             image_batch=[]
