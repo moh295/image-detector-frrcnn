d4c68da9f65e2b7d45b47b2b0dae096ead3908fa
moh295 <mohammed@clearimageai.com>
2022-07-20 16:06:09 +0200

    base trainner

--- trainner/utils.py
+++ trainner/utils.py
@@ -282,3 +282,14 @@ def init_distributed_mode(args):
     )
     torch.distributed.barrier()
     setup_for_distributed(args.rank == 0)
+
+
+def warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor):
+
+    def f(x):
+        if x >= warmup_iters:
+            return 1
+        alpha = float(x) / warmup_iters
+        return warmup_factor * (1 - alpha) + alpha
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, f)
